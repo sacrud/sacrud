ee6c4cb5ae8799436567accbf69d39e12cb721ff
Panov Sergey <spanovaleks@gmail.com>
2013-03-01 15:36:43 +0600

    update test

--- sacrud/pyramid_ext/tests.py
+++ sacrud/pyramid_ext/tests.py
@@ -5,7 +5,6 @@ class TestCreate(unittest.TestCase):
 
     def _makeRequest(self):
         request = testing.DummyRequest()
-        request.registry._zodb_databases = {'':DummyDB()}
         return request
     
     def test_test(self):
