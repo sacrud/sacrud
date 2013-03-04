9c86ad03b057f6ef8c03ce2f1853630706718cfd
Panov Sergey <spanov@spanov-System-Product-Name.(none)>
2013-03-04 15:39:33 +0600

    add basic tests

---
+++ sacrud/tests.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import unittest
+from sacrud.test_models import User, Profile
+from action import get_relations
+from sqlalchemy.schema import MetaData
+
+
+class SacrudTests(unittest.TestCase):
+
+    def setUp(self):
+        engine = create_engine('sqlite:///:memory:')
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        # You probably need to create some tables and 
+        # load some test data, do so here.
+        self.session = session
+        # To create tables, you typically do:
+        #User.metadata.create_all(engine)
+        User.metadata.create_all(engine)
+        Profile.metadata.create_all(engine)
+
+    def tearDown(self):
+        pass
+
+    def test_relations(self):
+
+        user = User(u'Vasya', u'Pupkin', u"123")
+
+        self.session.add(user)
+        self.session.commit()
+        profile = Profile(user=user)
+
+        self.session.add(profile)
+        self.session.commit()
+
+        profile = self.session.query(Profile).get(1)
+        self.assertEqual(get_relations(user), [('profile', [profile,])])
+ 
