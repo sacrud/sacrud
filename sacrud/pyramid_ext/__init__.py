fb9ef9f381ec0d9cda72e6971bfd4e0327333e5a
Panov Sergey <spanovaleks@gmail.com>
2013-03-06 10:39:38 +0600

    dont init session twice

--- sacrud/pyramid_ext/__init__.py
+++ sacrud/pyramid_ext/__init__.py
@@ -17,13 +17,13 @@ def add_routes(config):
 
 
 def includeme(config):
-    global DBSession
-    engine = sqlalchemy.engine_from_config(config.registry.settings)
-    if DBSession is None:
-        DBSession = orm.scoped_session(
-            orm.sessionmaker(extension=ZopeTransactionExtension()))
-    DBSession.remove()
-    DBSession.configure(bind=engine)
+    #global DBSession
+    #engine = sqlalchemy.engine_from_config(config.registry.settings)
+    #if DBSession is None:
+    #    DBSession = orm.scoped_session(
+    #        orm.sessionmaker(extension=ZopeTransactionExtension()))
+    #DBSession.remove()
+    #DBSession.configure(bind=engine)
 
     config.include('pyramid_jinja2')
     config.add_static_view('sa_static', 'sacrud:static')
