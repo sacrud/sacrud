d4d470a9304d020cbd2ae084f2281786c8a43eab
Dmitry <root@uralbash.ru>
2014-03-08 22:55:06 +0600

    inline fields and much more futures...

--- sacrud/action.py
+++ sacrud/action.py
@@ -81,7 +81,6 @@ def create(session, table, request=''):
         return
 
     pk_name = get_pk(table)
-    # TODO: переделать на sacrud_detailed_col or __table__.columns
     col = [c for c in table.sacrud_detail_col or table.__table__.columns]
     return {'pk': pk_name,
             'col': col,
