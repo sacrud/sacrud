32293700cd7915663e910ab9f439276fab9a8ca1
Dmitry <root@uralbash.ru>
2014-04-08 13:13:55 +0600

    test test

--- sacrud/tests/test_common.py
+++ sacrud/tests/test_common.py
@@ -1,13 +1,19 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2014 uralbash <root@uralbash.ru>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Test for sacrud.common
+"""
 
 import unittest
 
 
-class MockCGIFieldStorage(object):
-    pass
-
-
-class BaseSacrudTest(unittest.TestCase):
+class BaseTest(unittest.TestCase):
     def setUp(self):
         pass
 
@@ -15,7 +21,7 @@ class BaseSacrudTest(unittest.TestCase):
         pass
 
 
-class PaginatorTest(BaseSacrudTest):
+class PaginatorTest(BaseTest):
 
-    def test_relations(self):
+    def test_get_current_page(self):
         pass
