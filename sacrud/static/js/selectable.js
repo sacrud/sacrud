ca03a51e0ac930ffc6623337cd296342e181bbb3
nikita4477 <ggift@mail.ru>
2014-05-14 18:44:01 +0600

    sync checkboxes and selectable grid

--- sacrud/static/js/selectable.js
+++ sacrud/static/js/selectable.js
@@ -2,18 +2,39 @@ $(function() {
     $('table > tbody').selectable({
         // filter: ':not(td)',
         filter: 'tr',
-        cancel: 'a',
+        cancel: 'a, input',
         selected: function(event, ui) {
             $(ui.selected).addClass('table-tr-selected-class');
+            $(ui.selected).find('input[type="checkbox"]').prop('checked', true);
             // console.log(ui.selected);
             // console.log($(this).data('uiSelectable').selectees.filter('.ui-selected'));
         },
         unselected: function(event, ui) {
             $(ui.unselected).removeClass('table-tr-selected-class');
+            $(ui.unselected).find('input[type="checkbox"]').prop('checked', false);
         },
         // create: function(event, ui) {},
         // start: function(event, ui) {},
         // selecting: function(event, ui) {},
         // stop: function(event, ui) {},
+    });
+
+    function check_checkbox (checkbox) {
+        var $parent_tr = checkbox.parents('.sacrud-grid-content-grid__body-row');
+        if (checkbox.prop('checked')) {
+            $parent_tr.addClass('table-tr-selected-class');
+            $parent_tr.addClass('ui-selected');
+        } else {
+            $parent_tr.removeClass('table-tr-selected-class');
+            $parent_tr.removeClass('ui-selected');
+        }
+    }
+
+    $(document).on('change', 'input[type="checkbox"]' , function (event) {
+        check_checkbox($(this));
+    });
+
+    $('input[type="checkbox"]:checked').each(function(){
+        check_checkbox($(this));
     });
 });
