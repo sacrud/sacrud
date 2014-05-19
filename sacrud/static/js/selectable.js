ab8d5283092e107d286b1989f66792c39f363b2b
nikita4477 <ggift@mail.ru>
2014-05-19 19:38:09 +0600

    fix select all checkboxes

--- sacrud/static/js/selectable.js
+++ sacrud/static/js/selectable.js
@@ -3,6 +3,7 @@ $(function() {
     var options = {
         'tr_selected_class': 'table-tr-selected-class',
         'state_disable_class': 'toolbar-button__item_state_disable',
+        'all_checkboxes_button': '#selected_all_item',
         'table_checkboxes': 'input[name="selected_item"]',
         'table_checkboxes_checked': 'input[name="selected_item"]:checked',
         'input_selected_action': 'input[name="selected_action"]',
@@ -17,6 +18,7 @@ $(function() {
         selected: function (event, ui) {
             $(ui.selected).addClass(options.tr_selected_class);
             $(ui.selected).find(options.table_checkboxes).prop('checked', true).change();
+            $(options.all_checkboxes_button).prop('checked', false);
             // console.log(ui.selected);
             // console.log($(this).data('uiSelectable').selectees.filter('.ui-selected'));
         },
@@ -26,11 +28,12 @@ $(function() {
         },
     });
 
-    function change_button () {
+    function change_buttons () {
         if ($(options.table_checkboxes_checked).length) {
             $('.'+options.state_disable_class).removeClass(options.state_disable_class);
         } else {
             $(options.div_action_button).addClass(options.state_disable_class);
+            $(options.all_checkboxes_button).prop('checked', false);
         }
     }
 
@@ -49,18 +52,18 @@ $(function() {
         check_checkbox($(this));
     });
 
-    change_button();
+    change_buttons();
 
     $(document).on('change', options.table_checkboxes , function () {
         check_checkbox($(this));
-        change_button();
+        change_buttons();
     });
 
     $(document).on('click', '.'+options.state_disable_class , function (event) {
         event.stopImmediatePropagation();
     });
 
-    $(document).on('change', '#selected_all_item' , function () {
+    $(document).on('change', options.all_checkboxes_button , function () {
         $(options.table_checkboxes).prop('checked', $(this).prop('checked')).change();
     });
 
