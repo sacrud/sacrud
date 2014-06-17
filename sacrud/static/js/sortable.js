1fe277f0a11321c552ef6b20f54fe87bdff47e82
Dmitry <root@uralbash.ru>
2014-06-17 15:29:52 +0600

    fix sortable dashbord

--- sacrud/static/js/sortable.js
+++ sacrud/static/js/sortable.js
@@ -1,4 +1,21 @@
 $(function() {
+    function move(event, ui) {
+      var widget = ui.item.attr('name'),
+          column = ui.item.parent().data('number'),
+          position = $('.dashboard__column[data-number="'+column+'"]  .widget').index(ui.item),
+          data = {'widget': widget, 'column': column, 'position': position};
+
+      $.ajax({
+          type: "POST",
+          url: 'save_position',
+          data: data,
+          // success: function(result){},
+          error: function (xhr, textStatus, errorThrown) {
+              ui.sender.sortable("cancel");
+          }
+      });
+    }
+
     $('.sortable').sortable({
         connectWith: '.sortable',
         placeholder: 'widget_placeholder',
@@ -7,21 +24,8 @@ $(function() {
             $('.widget_placeholder').append(ui.item.html());
             // $('.widget_placeholder').append(ui.item.context.outerHTML);
         },
-        receive: function(event, ui) {
-            var widget = ui.item.attr('name'),
-                column = ui.item.parent().data('number'),
-                position = $('.dashboard__column[data-number="'+column+'"]  .widget').index(ui.item),
-                data = {'widget': widget, 'column': column, 'position': position};
-
-            $.ajax({
-                type: "POST",
-                url: 'save_position',
-                data: data,
-                // success: function(result){},
-                error: function (xhr, textStatus, errorThrown) {
-                    ui.sender.sortable("cancel");
-                }
-            });
+        beforeStop: function(event, ui) {
+          move(event, ui);
         },
         // stop: function(event, ui) {},
         // change: function(event, ui) {},
