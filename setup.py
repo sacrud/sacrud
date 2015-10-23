ddb26972fb173019acf8f595df08a5db7879af6a
Dmitry <root@uralbash.ru>
2015-10-23 13:43:09 +0500

    delete exttype module #105

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(name):
 
 setup(
     name='sacrud',
-    version='0.3.3',
+    version='0.3.4.dev1',
     url='http://github.com/ITCase/sacrud/',
     author='Svintsov Dmitry',
     author_email='root@uralbash.ru',
